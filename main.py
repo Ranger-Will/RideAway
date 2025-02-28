0919c3e67a2e7b686a56e137bb1b310e2f1847fe
Ranger-Will <103150019+Ranger-Will@users.noreply.github.com>
2025-02-28 11:52:34 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import pygame
-
+import sys
 
 class Game:
   def __init__():
+    pygame.init
+    pygame.display.set_caption("Ride Away")
+    display = pygame.display.set_mode((320, 640))
+    clock = pygame.time.Clock()
+
+  def run():
+    while True:
+
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          pygame.quit()
+          sys.exit()
+
+
+      pygame.display.update()
+      clock.tick = 60
     
+Game().run()
